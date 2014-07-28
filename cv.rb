eb60b02b0c2cfd79793c86eef9fe9f9789aa0499
Luke Byrne <me@lukebyrne.com>
2014-07-28 23:00:04 +0100

    Updating.

--- cv.rb
+++ cv.rb
@@ -6,7 +6,12 @@ module CV
 
     def self.output
       <<-END
+
+      `If this README.md is a little lite for you, jump into the cv.rb that creates this file. You may enjoy some of the code or comments.`
+
       My goal is to secure a short to medium term contract working in either AngularJS, Sinatra / Ruby on Rails.
+
+      I like the Sinatra/NodeJS micro services approach on the backend to build up an API, and AngularJS on the front end to create a compelling UI/UX.
 
       My preference is for greenfield projects with a Service Orientated Architecture (SOA).
 
